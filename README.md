29a0caa22d60eda6e4b17e3211c2f5ff0b862aa6
Mikhail <m.zherdev@tcombank.ru>
2018-02-26 12:24:59 +0300

    strength

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+The program valuates strength of the given password.
+
+# How to use
+Run from command line:
+```shell
+cd ${path_to_script}
+python password_strength.py <password>
+```
+Example of output:
+
+```shell
+Password strength: 3
+```
+
+# How to run.
+
+For correct work of script Python 3.5 is required.
+
+Run on Linux/Windows:
+
+```bash
+$ python password_strength.py <password> # possibly requires call of python3 executive instead of just python
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,25 @@
-
+import sys
+import re
 
 def get_password_strength(password):
-    pass
+    if len(password) == 0:
+        return 0
+
+    strength = 1
+    if len(password) > 8:
+        strength += 2
+    elif 6 <= len(password) <= 8:
+        strength += 1
+    if not password.isdigit():
+        strength += 1
+        if re.search('\d', password):
+            strength += 2
+    if not (password.isupper() or password.islower()):
+        strength += 2
+    if re.search('[@#$]', password):
+            strength += 2
+    return strength
 
 
 if __name__ == '__main__':
-    pass
+    print('Password strength: %s' % get_password_strength(sys.argv[1]))
